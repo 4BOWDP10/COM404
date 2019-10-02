1aed6a2aa82c3ff8611e0771dd5873195559d4a8
4BOWDP10 <4BOWDP10@solent.ac.uk>
2019-10-02 11:19:59 +0000

    Added code to demonstrate the use of comparison operators.

---
+++ 0-setup/1-basics/1-output/1-simple-message/1-basics/1-output/2-multiline-message/1-basics/1-output/3-escape-characters/1-basics/1-output/4-ascii-art/1-basics/2-input/1-user-input/1-basics/3-decision/1-if/1-basics/3-decision/2-if-else/1-basics/3-decision/3-if-elif-else/1-basics/3-decision/4-modulo-operator/1-basics/3-decision/5-comparison-operators/bot.py
@@ -0,0 +1,10 @@
+print ("please enter the first number")
+firstNumber = int(input())
+print ("please enter the second number")
+secondNumber = int(input())
+if firstNumber > secondNumber: 
+    print ("the first number is bigger!")
+elif firstNumber == secondNumber:
+    print ("the numbers are the same!")   
+elif firstNumber < secondNumber:
+    print ("the second number is bigger!")     
