cdddd01c3f20415345f845f147d817b998623c1f
4BOWDP10 <4BOWDP10@solent.ac.uk>
2019-10-02 09:25:30 +0000

    Added code to display an ascii art robot

---
+++ 0-setup/1-basics/1-output/1-simple-message/1-basics/1-output/2-multiline-message/1-basics/1-output/3-escape-characters/1-basics/1-output/4-ascii-art/bot.py
@@ -0,0 +1,19 @@
+# Display a box
+print("##########")
+print("#        #")
+print("#        #")
+print("##########")
+
+# Display an ascii art robot using long string
+print(""" 
+##########
+#  o  o  #
+#  ----  #
+##########
+""")
+
+# Display an ascii art robot
+print("##########")
+print("#  o  o  #")
+print("#  ----  #")
+print("##########")
